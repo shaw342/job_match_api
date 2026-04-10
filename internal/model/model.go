ad01d932f5d168e7a5b7ad0fae3fa023b2d43841
shawan.barua <shawan.barua@ecole2600.com>
2026-04-10 15:16:49 +0200

    create data model

--- internal/model/model.go
+++ internal/model/model.go
@@ -4,3 +4,11 @@ type AnalyzeRequest struct {
 	JobDescription string `json:"job_description"`
 	CVText         string `json:"cv_text"`
 }
+
+type AnalysisResult struct {
+	MatchScore      int      `json:"match_score"`
+	Summary         string   `json:"summary"`
+	MatchedSkills   []string `json:"matched_skills"`
+	MissingSkills   []string `json:"missing_skills"`
+	Recommendations []string `json:"recommendations"`
+}
